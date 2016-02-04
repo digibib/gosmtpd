5da5db822b4f06733c8ad5a80030cbb5d783b27e
Benjamin Rokseth <bensinober@gmail.com>
2016-02-04 15:14:37 +0100

    Log error on forwardEmail

--- forward.go
+++ forward.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"log"
 	"net"
 	"net/smtp"
 )
 
+// fake Auth by returning empty
+type nillAuth struct{}
+
+func (n nillAuth) Start(server *smtp.ServerInfo) (proto string, toServer []byte, err error) {
+	return
+}
+func (n nillAuth) Next(fromServer []byte, more bool) (toServer []byte, err error) {
+	return
+}
+
 func getForwardHost() string {
 	return net.JoinHostPort(*forwardhost, *forwardport)
 }
-func forwardEmail(client *MailConnection) {
-	smtp.SendMail(getForwardHost(), nil, client.From, []string{client.To}, []byte(client.Data))
 
+func forwardEmail(client *MailConnection) {
+	err := smtp.SendMail(*forwardsmtp, nillAuth{}, client.From, []string{client.To}, []byte(client.Data))
+	if err != nil {
+		log.Fatal("ERROR %v", err)
+	}
 }
